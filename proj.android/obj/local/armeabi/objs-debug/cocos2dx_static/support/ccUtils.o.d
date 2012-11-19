bef21fcbb3e825f2a8573e0adb5eec110dd3292f
woozzu <woozzu@gmail.com>
2012-11-19 22:58:17 +0900

    complete module commit

--- proj.android/obj/local/armeabi/objs-debug/cocos2dx_static/support/ccUtils.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs-debug/cocos2dx_static/support/ccUtils.o: \
- /cygdrive/d/developement/cocos2d-2.0-x-2.0.4/Realfighter/proj.android/../../cocos2dx/support/ccUtils.cpp \
- /cygdrive/d/developement/cocos2d-2.0-x-2.0.4/Realfighter/proj.android/../../cocos2dx/support/ccUtils.h
-
-/cygdrive/d/developement/cocos2d-2.0-x-2.0.4/Realfighter/proj.android/../../cocos2dx/support/ccUtils.h:
